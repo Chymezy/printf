32d237d4d5dc855c31d764de2afed8a8da741227
Chymezy <chymezy332@gmail.com>
2023-09-17 16:20:10 -0700

    task_0_sourceCodes.c: deleted because line is above 40 lines

---
+++ task_0_sourceCode.c
@@ -0,0 +1,153 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h> /* Include the stdlib.h header for exit function */
+
+/* Forward declarations */
+int print_format(const char *format, va_list args);
+int handle_format_specifier(char specifier, va_list args);
+int print_number(int num);
+int print_string(const char *str);
+
+/**
+ * _printf - Custom printf function
+ * @format: The format string
+ * Return: Number of characters printed (excluding null byte) on success,
+ *         -1 on error, and it may exit the program on critical error
+ */
+int _printf(const char *format, ...)
+{
+	int printed_chars = 0;
+	va_list args;
+
+	va_start(args, format);
+	printed_chars = print_format(format, args);
+	va_end(args);
+
+	return (printed_chars);
+}
+
+/**
+ * print_format - Print the formatted output based on the format string
+ * @format: The format string
+ * @args: The variable argument list
+ * Return: Number of characters printed
+ */
+int print_format(const char *format, va_list args)
+{
+	int printed_chars = 0;
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			putchar(*format);
+			printed_chars++;
+		}
+		else
+		{
+			format++; /* Move past the '%' */
+			if (*format == '\0')
+			{
+				putchar('%');
+				return (-1);
+			}
+			printed_chars += handle_format_specifier(*format, args);
+		}
+		format++;
+	}
+
+	return (printed_chars);
+}
+
+/**
+ * handle_format_specifier - Handle a format specifier
+ * @specifier: The format specifier character
+ * @args: The variable argument list
+ * Return: Number of characters printed for the specifier
+ */
+int handle_format_specifier(char specifier, va_list args)
+{
+	if (specifier == 'c')
+	{
+		int c = va_arg(args, int);
+
+		putchar(c);
+		return (1);
+	}
+	else if (specifier == 's')
+	{
+		char *str = va_arg(args, char *);
+
+		if (str != NULL)
+			return (print_string(str));
+	}
+	else if (specifier == '%')
+	{
+		putchar('%');
+		return (1);
+	}
+	else if (specifier == 'd' || specifier == 'i')
+	{
+		return (print_number(va_arg(args, int)));
+	}
+
+	putchar('%');
+	putchar(specifier);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * print_number - Print an integer to stdout
+ * @num: The integer to be printed
+ * Return: The number of characters printed
+ */
+int print_number(int num)
+{
+	int printed_chars = 0;
+
+	if (num < 0)
+	{
+		putchar('-');
+		printed_chars++;
+		num = -num;
+	}
+
+	if (num == 0)
+	{
+		putchar('0');
+		printed_chars++;
+	}
+	else
+	{
+		while (num != 0)
+		{
+			putchar('0' + (num % 10));
+			printed_chars++;
+			num /= 10;
+		}
+	}
+
+	return (printed_chars);
+}
+
+/**
+ * print_string - Print a string to stdout
+ * @str: The string to be printed
+ * Return: The number of characters printed
+ */
+int print_string(const char *str)
+{
+	int printed_chars = 0;
+
+	while (*str)
+	{
+		putchar(*str);
+		printed_chars++;
+		str++;
+	}
+
+	return (printed_chars);
+}
+
