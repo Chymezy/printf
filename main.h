d1e31eafd59f17b0437292a80bcf63b28c96d3ca
Chymezy <chymezy332@gmail.com>
2023-09-17 16:24:10 -0700

    main.h: update with chuks of new function prototypes create to reduce the number of lines

--- main.h
+++ main.h
@@ -2,5 +2,9 @@
 #define MAIN_H
 
 int _printf(const char *format, ...);
+int print_format(const char *format, va_list args);
+int handle_format_specifier(char specifier, va_list args);
+int print_number(int num);
+int print_string(const char *str);
 
 #endif /* contains header files */
