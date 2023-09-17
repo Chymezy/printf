5b9fbbf4404bccc991b6800eb69f6910d57720a2
Chymezy <chymezy332@gmail.com>
2023-09-17 14:21:48 -0700

    task_0_sourceCodes.c updated: source code not conforming with betty requirement and thus updated adequately by removing white spaceson line 16 and 55 respectively

--- task_0_sourceCodes.c
+++ task_0_sourceCodes.c
@@ -13,7 +13,7 @@ int _printf(const char *format, ...)
 {
 	int printed_chars = 0;
 	va_list args;
-	
+
 	va_start(args, format);
 
 	while (*format)
@@ -52,4 +52,4 @@ int _printf(const char *format, ...)
 	}
 	va_end(args);
 	return (printed_chars);
-} 
+}
