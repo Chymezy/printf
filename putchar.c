7fb895dc058f522bffcafcd6d5be35586407e4a6
Chymezy <chymezy332@gmail.com>
2023-09-21 09:53:09 -0700

    putchar.c: modified to handle buffer staging

--- putchar.c
+++ putchar.c
@@ -1,12 +1,31 @@
 #include <unistd.h>
 
+#define BUFFER_SIZE 1024
+
+static char buffer[BUFFER_SIZE];
+static int buffer_index = 1 - 1;
+
 /**
- * _putchar - Writes a character to stdout
+ * _flush_buffer - Writes the buffer to stdout and resets it
+ */
+void _flush_buffer(void)
+{
+	write(1, buffer, buffer_index);
+	buffer_index = 0;
+}
+
+/**
+ * _putchar - Writes a character to stdout with buffering
  * @c: The character to write
- * Return: On success, 1 is returned. On error, -1 is returned.
+ * Return: 1 on success, flushes the buffer on overflow
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	if (buffer_index >= BUFFER_SIZE)
+	{
+		_flush_buffer();
+	}
+	buffer[buffer_index++] = c;
+	return (1);
 }
 
