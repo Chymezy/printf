efd45ab35e2bb26a1307945e18dd0eb3ca691756
Chymezy <chymezy332@gmail.com>
2023-09-21 09:51:49 -0700

    printf.c: updated to include additional format specifers and bufferhandling

--- printf.c
+++ printf.c
@@ -1,25 +1,27 @@
 #include "main.h"
-#include <stdint.h>
 
 /**
  * _printf - Custom printf function
- * @format: The format string
- * Return: Number of characters printed (excluding null byte) on success,
- *         -1 on error, and it may exit the program on critical error
+ *
+ * This function takes a format string and a variable number of arguments and
+ * prints formatted output based on the format specifier in the format string.
+ *
+ * @format: The format string containing format specifiers.
+ * @...: Variable number of arguments corresponding to format specifiers.
+ *
+ * Return: The total number of characters printed.
  */
 int _printf(const char *format, ...)
 {
 	int printed_chars;
 
-	conver_t funct_list[] = {
-		{"c", p_char},
-		{"s", p_string},
-		{"%", p_percent},
-		{"d", p_integer},
-		{"i", p_integer},
-		{"b", p_binary},
-		{NULL, NULL}
-	};
+	conver_t funct_list[] = {{"c", p_char}, {"s", p_string},
+							{"%", p_percent}, {"d", p_integer},
+							{"i", p_integer}, {"u", print_unsigned_2},
+							{"x", print_hex_lower}, {"X", print_hex_upper},
+							{"p", print_pointer}, {"S", print_custom_string},
+							{"b", p_binary}, {"o", p_octal},
+							{NULL, NULL}};
 
 	va_list args;
 
@@ -28,10 +30,13 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	/** call a parser function */
+	/* Call a parser function */
 
 	printed_chars = parser(format, funct_list, args);
 	va_end(args);
+
+	/* Flush any remaining characters in the buffer */
+	_flush_buffer();
 
 	return (printed_chars);
 }
